a942fa8aa7e242e998b94821bfed273f8bb649c6
Olivia Liao <ol64@cornell.edu>
2022-07-09 02:22:01 -0400

    leaflet setup

--- app.js
+++ app.js
@@ -37,3 +37,10 @@ document.addEventListener('click', hideMenu);
 // };
 
 // menuLinks.addEventListener('click', toggleActivePage);
+
+//Map
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '© OpenStreetMap',
+}).addTo(map);
