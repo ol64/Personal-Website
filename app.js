04a36172a5495de2d2b7d68ec7b7d4717df08f17
Olivia Liao <ol64@cornell.edu>
2022-07-14 02:25:12 -0400

    added more details to travel map

--- app.js
+++ app.js
@@ -45,11 +45,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '© OpenStreetMap',
 }).addTo(map);
 
-var NYC = L.marker([40.6932363, -73.9848]).addTo(map);
+var newYorkCity = L.marker([40.6932363, -73.9848]).addTo(map);
 var amsterdam = L.marker([52.3770361, 4.8986806]).addTo(map);
 var salamanca = L.marker([40.9650389, -5.6643833]).addTo(map);
 var madrid = L.marker([40.4222528, -3.6996306]).addTo(map);
-var rome = L.marker([41.8901301, 12.4726551]).addTo(map);
+var roma = L.marker([41.8901301, 12.4726551]).addTo(map);
 var positano = L.marker([40.627675, 14.4853502]).addTo(map);
 var porto = L.marker([41.1378444, -8.6131083]).addTo(map);
 var london = L.marker([51.5067556, -0.084875]).addTo(map);
@@ -57,37 +57,44 @@ var barcelona = L.marker([41.3893556, 2.1864472]).addTo(map);
 var santander = L.marker([43.4663556, -3.8051806]).addTo(map);
 var cancun = L.marker([20.5862722, -87.1101306]).addTo(map);
 var sanDiego = L.marker([32.7312278, -117.1500694]).addTo(map);
+var biscay = L.marker([43.4454194, -2.7841083]).addTo(map);
+var firenze = L.marker([43.7632139, 11.2638194]).addTo(map);
+var bilbao = L.marker([43.2691, -2.9346417]).addTo(map);
+var washington = L.marker([38.8895944, -77.0153417]).addTo(map);
 
 // home
 // 	.bindPopup("<img src='destinations/nyc.jpeg' style='width: auto'/>NYC, NY")
 // 	.openPopup();
 
 var markers = {
-	NYC: NYC,
-	amsterdam: amsterdam,
-	salamanca: salamanca,
-	madrid: madrid,
-	rome: rome,
-	positano: positano,
-	porto: porto,
-	london: london,
-	barcelona: barcelona,
-	santander: santander,
-	cancun: cancun,
-	sanDiego: sanDiego,
+	newYorkCity: [newYorkCity, 'New York'],
+	amsterdam: [amsterdam, 'Netherlands'],
+	salamanca: [salamanca, 'Spain'],
+	madrid: [madrid, 'Spain'],
+	roma: [roma, 'Italy'],
+	positano: [positano, 'Italy'],
+	porto: [porto, 'Portugal'],
+	london: [london, 'UK'],
+	barcelona: [barcelona, 'Spain'],
+	santander: [santander, 'Spain'],
+	cancun: [cancun, 'Mexico'],
+	sanDiego: [sanDiego, 'California'],
+	biscay: [biscay, 'Spain'],
+	firenze: [firenze, 'Italy'],
+	bilbao: [bilbao, 'Spain'],
+	washington: [washington, 'D.C.'],
 };
 
 let image = document.getElementById('destination');
 let caption = document.querySelector('.polaroid h3');
 
 for (const destination in markers) {
-	markers[destination].on('click', function () {
-		image.classList.add('fade-in-image');
+	markers[destination][0].on('click', function () {
 		image.src = `destinations/${destination}.jpeg`;
 
 		let location = destination.split('');
 		caption.innerHTML = `${
 			location.shift().toUpperCase() + location.join('')
-		} `;
+		}, ${markers[destination][1]}`;
 	});
 }
